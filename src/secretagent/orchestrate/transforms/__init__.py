1fc53de6cce3d97d0e2510b50bdb6de7a8d1d4f4
Aditya Kumar <adityakumar2001@outlook.com>
2026-04-13 00:20:42 -0400

    register swap_strategy, upgrade, route, crossover transforms

--- src/secretagent/orchestrate/transforms/__init__.py
+++ src/secretagent/orchestrate/transforms/__init__.py
@@ -11,6 +11,10 @@ from secretagent.orchestrate.transforms.expand import ExpandTransform
 from secretagent.orchestrate.transforms.repair import RepairTransform
 from secretagent.orchestrate.transforms.restructure import RestructureTransform
 from secretagent.orchestrate.transforms.evolve import EvolveTransform
+from secretagent.orchestrate.transforms.swap_strategy import SwapStrategyTransform
+from secretagent.orchestrate.transforms.upgrade import UpgradeTransform
+from secretagent.orchestrate.transforms.route import RouteTransform
+from secretagent.orchestrate.transforms.crossover import CrossoverTransform
 
 register_transform('prune', PruneTransform())
 register_transform('downgrade', DowngradeTransform())
@@ -19,3 +23,7 @@ register_transform('expand', ExpandTransform())
 register_transform('repair', RepairTransform())
 register_transform('restructure', RestructureTransform())
 register_transform('evolve', EvolveTransform())
+register_transform('swap_strategy', SwapStrategyTransform())
+register_transform('upgrade', UpgradeTransform())
+register_transform('route', RouteTransform())
+register_transform('crossover', CrossoverTransform())
